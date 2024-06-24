feaf5979d671e8231127e00813790212f1ae0722
Filip Trajkovski <filip.trajkovski@sanitas.com>
2024-06-24 21:48:52 +0200

    Final solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import {useState} from "react";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 1000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    })
+
+    const inputIsValid = userInput.duration >= 1;
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevData => {
+            return {
+                ...prevData,
+                [inputIdentifier]: +newValue
+            }
+        })
+    }
+
+    return (
+        <>
+            <Header/>
+            <UserInput onChange={handleChange} userInput={userInput}/>
+            {!inputIsValid && <p className="center">Please input valid year</p>}
+            {inputIsValid && <Results userInput={userInput}></Results>}
+        </>
+    )
 }
 
 export default App
